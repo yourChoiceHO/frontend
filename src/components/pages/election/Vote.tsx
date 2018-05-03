ccd1cf16107b06a3d8bd0c8f5e196ad6aa0df948
Jonas Hauß <haussjonas@gmail.com>
2018-05-03 12:41:25 +0200

    initial commit. added a reference to the necessary model

--- src/components/pages/election/Vote.tsx
+++ src/components/pages/election/Vote.tsx
@@ -2,6 +2,7 @@ import React, { SFC } from "react";
 
 import Header from "@/components/organisms/Header";
 import PageTemplate from "@/components/templates/Page";
+// import { IElectionEntity } from "@/types/model";
 
 const ElectionVotePage: SFC<{}> = () => {
   return (
