91e4f8fe18e490488f39e58e9cfe1982c7b8eae3
Jonas K <jkirchho@stud.hs-offenburg.de>
2018-05-08 22:34:14 +0200

    Detail page enhanced

--- src/components/pages/election/Detail.tsx
+++ src/components/pages/election/Detail.tsx
@@ -1,33 +1,47 @@
-import moment from "moment";    
+import moment from "moment";
 
 import React, { SFC } from "react";
 
 import Header from "@/components/organisms/Header";
 import PageTemplate from "@/components/templates/Page";
 
-import { IElectionEntity } from "@/types/model";   
+// import { IElectionEntity } from "@/types/model";
+
+import { IElectionEntity } from "@/types/model";
+
+import { Button, Card } from "antd";
+const ButtonGroup = Button.Group;
 
 const election: IElectionEntity = {
   client_id: 2,
-  end_date: moment("07062018", "DDMMYYYY"), 
+  end_date: moment("07062018", "DDMMYYYY"),
   id_election: 1,
-  start_date: moment(), 
+  start_date: moment(),
   state: 7,
-  text: "Sampletext Sampletext Sampletext Sampletext Sampletext Sampletext", 
+  text: "Sampletext Sampletext Sampletext Sampletext Sampletext Sampletext",
   type: "Bundestagswahl"
 };
 
 const ElectionDetailPage: SFC<{}> = () => {
   return (
     <PageTemplate header={<Header />}>
-      <div>Details zur {election.type} </div>
-
-      <li> {election.text} </li>
-      <li>Wahl ID: {election.id_election} </li>
-      <li>Client ID: {election.client_id} </li>
-      <li>Zustand: {election.state} </li>
-      <li>startet am: {election.start_date.format("LLL")} </li>
-      <li>endet am: {election.end_date.format("LLL")} </li>
+      <div style={{ background: "#ECECEC", padding: "30px" }}>
+        <Card title={election.type} bordered={true} style={{ width: 300 }}>
+          <p>{election.text}</p>
+          <p>Wahl ID: {election.id_election}</p>
+          <p>Client ID: {election.client_id}</p>
+          <p>Zustand: {election.state}</p>
+          <p>startet am: {election.start_date.format("LLL")}</p>
+          <p>endet am: {election.end_date.format("LLL")}</p>
+        </Card>
+      </div>
+      <div>
+        <Button>Bearbeiten</Button>
+        <ButtonGroup style={{ padding: "30px" }}>
+          <Button>Speichern</Button>
+          <Button>Abbrechen</Button>
+        </ButtonGroup>
+      </div>
     </PageTemplate>
   );
 };
