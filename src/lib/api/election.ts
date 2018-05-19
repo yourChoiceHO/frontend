6d3db6dceb76e145347149f8967ed3134e4ff89b
Jonas Hauß <haussjonas@gmail.com>
2018-05-19 17:04:52 +0200

    Fake api backend was added. This replaces the axios mock adapter. The backend contains a fake database with preinserted fake data sets.

--- src/lib/api/election.ts
+++ src/lib/api/election.ts
@@ -2,21 +2,21 @@ import * as http from "@/lib/http";
 import { IElectionEntity } from "@/types/model";
 
 export function create(election: Partial<IElectionEntity>) {
-  return http.post<IElectionEntity>("/elections", election);
+  return http.post<IElectionEntity>("/election", election);
 }
 
 export function remove(id: number) {
-  return http.del<IElectionEntity>(`/elections/${id}`);
+  return http.del<IElectionEntity>(`/election/${id}`);
 }
 
 export function getAll() {
-  return http.get<IElectionEntity[]>("/elections");
+  return http.get<IElectionEntity[]>("/election");
 }
 
 export function get(id: number) {
-  return http.get<IElectionEntity>(`/elections/${id}`);
+  return http.get<IElectionEntity>(`/election/${id}`);
 }
 
 export function update(id: number, updates: Partial<IElectionEntity> = {}) {
-  return http.put<IElectionEntity>(`/elections/${id}`, updates);
+  return http.put<IElectionEntity>(`/election/${id}`, updates);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -106,6 +106,9 @@ module.exports = {
     overlay: {
       warnings: true,
       errors: true
+    },
+    proxy: {
+      "/api": "http://localhost:3001"
     }
   },
   resolve: {
