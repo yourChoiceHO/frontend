2c40bf8545f4a57abd6697775b7d87f3d0972d27
Matze <kaismann@web.de>
2018-05-24 02:04:55 +0200

    form reaction now on different radio button input

--- src/components/pages/election/Create.tsx
+++ src/components/pages/election/Create.tsx
@@ -10,21 +10,20 @@ import {
   TimePicker,
   Upload
 } from "antd";
-// import CSSProperties= ;
+import classNames from "classnames/bind";
 import Moment from "moment";
 import React, { Component, CSSProperties } from "react";
+
+import Styles from "@/components/pages/styles/hidden_and_show.less";
 
 import Header from "@/components/organisms/Header";
 import PageTemplate from "@/components/templates/Page";
 import { FormComponentProps } from "antd/lib/form";
+const cx = classNames.bind(Styles);
 // const election: IElectionEntity;
 const FormItem = Form.Item;
 const RadioGroup = Radio.Group;
 const format = "HH:mm";
-/*const gridStyle = {
-  textAlign: "center",
-  width: "25%"
-};*/
 const gridStyle: CSSProperties = {
   textAlign: "justify",
   width: "100%"
@@ -39,13 +38,8 @@ interface IUserFormProps extends FormComponentProps {
   type: string;
 }
 class ElectionCreatePage extends Component<IUserFormProps, {}> {
-  public radioStyle = {
-    display: "block",
-    height: "30px",
-    lineHeight: "30px"
-  };
-
   public state = {
+    hidden: 0,
     value: 1
   };
   public handleSubmit = (e: any) => {
@@ -65,18 +59,80 @@ class ElectionCreatePage extends Component<IUserFormProps, {}> {
       height: "30px",
       lineHeight: "30px"
     };
+
+    if (this.state.value === 2 || this.state.value === 3) {
+      const e2 = document.getElementsByClassName(cx("hidden2"))[0];
+      if (e2) {
+        e2.className = cx("show2");
+      }
+
+      const e3 = document.getElementsByClassName(cx("hidden3"))[0];
+      if (e3) {
+        e3.className = cx("show3");
+      }
+      const e4 = document.getElementsByClassName(cx("show4"))[0];
+      if (e4 != null) {
+        e4.className = cx("hidden4");
+      }
+    } else if (
+      this.state.value === 4 ||
+      this.state.value === 6 ||
+      this.state.value === 7
+    ) {
+      const e2 = document.getElementsByClassName(cx("show2"))[0];
+
+      if (e2) {
+        e2.className = cx("hidden2");
+      }
+      const e3 = document.getElementsByClassName(cx("hidden3"))[0];
+      if (e3) {
+        e3.className = cx("show3");
+      }
+      const e4 = document.getElementsByClassName(cx("show4"))[0];
+      if (e4 != null) {
+        e4.className = cx("hidden4");
+      }
+    } else if (this.state.value === 1) {
+      const e2 = document.getElementsByClassName(cx("hidden2"))[0];
+      if (e2 != null) {
+        e2.className = cx("show2");
+      }
+      const e3 = document.getElementsByClassName(cx("show3"))[0];
+      if (e3 != null) {
+        e3.className = cx("hidden3");
+      }
+      const e4 = document.getElementsByClassName(cx("show4"))[0];
+      if (e4 != null) {
+        e4.className = cx("hidden4");
+      }
+    } else if (this.state.value === 5) {
+      const e2 = document.getElementsByClassName(cx("show2"))[0];
+      if (e2 != null) {
+        e2.className = cx("hidden2");
+      }
+      const e3 = document.getElementsByClassName(cx("show3"))[0];
+      if (e3 != null) {
+        e3.className = cx("hidden3");
+      }
+      const e4 = document.getElementsByClassName(cx("hidden4"))[0];
+      if (e4 != null) {
+        e4.className = cx("show4");
+      }
+    }
     const { getFieldDecorator } = this.props.form;
     return (
       <PageTemplate header={<Header />}>
-        <div>Neue Wahl</div>
         <Form onSubmit={this.handleSubmit} layout="horizontal">
           <Card title="Wahl erstellen">
             <Row>
-              <Col span={8}>
+              <Col span={8} className={cx("std")}>
                 <FormItem>
                   {getFieldDecorator("type", {
                     rules: [
-                      { required: true, message: "Bitte Wahltyp auswählen!" }
+                      {
+                        message: "Bitte Wahltyp auswählen!",
+                        required: true
+                      }
                     ]
                   })(
                     <Card.Grid style={gridStyle}>
@@ -110,13 +166,13 @@ class ElectionCreatePage extends Component<IUserFormProps, {}> {
                   )}
                 </FormItem>
               </Col>
-              <Col span={8}>
+              <Col span={8} className={cx("std")}>
                 <FormItem>
                   {getFieldDecorator("start_date", {
                     rules: [
                       {
-                        required: true,
-                        message: "Bitte Startzeitpunkt festlegen!"
+                        message: "Bitte Startzeitpunkt festlegen!",
+                        required: true
                       }
                     ]
                   })(
@@ -131,13 +187,13 @@ class ElectionCreatePage extends Component<IUserFormProps, {}> {
                   )}
                 </FormItem>
               </Col>
-              <Col span={8}>
+              <Col span={8} className={cx("std")}>
                 <FormItem>
-                  {getFieldDecorator("start_date", {
+                  {getFieldDecorator("end_date", {
                     rules: [
                       {
-                        required: true,
-                        message: "Bitte Endzeitpunkt festlegen!"
+                        message: "Bitte Endzeitpunkt festlegen!",
+                        required: true
                       }
                     ]
                   })(
@@ -155,67 +211,92 @@ class ElectionCreatePage extends Component<IUserFormProps, {}> {
             </Row>
             <Row>
               <Col span={8}>
-                <FormItem>
-                  {getFieldDecorator("start_date", {
-                    rules: [
-                      {
-                        required: true,
-                        message: "Bitte Wählerliste importieren!"
-                      }
-                    ]
-                  })(
-                    <Card.Grid style={gridStyle}>
-                      {" "}
-                      <Upload {...this.props.form}>
-                        <Button>
-                          <Icon type="upload" /> Wählerliste importieren
-                        </Button>
-                      </Upload>
-                    </Card.Grid>
-                  )}
-                </FormItem>
+                <div className={cx("show1")}>
+                  <FormItem>
+                    {getFieldDecorator("voters", {
+                      rules: [
+                        {
+                          message: "Bitte Wählerliste importieren!",
+                          required: true
+                        }
+                      ]
+                    })(
+                      <Card.Grid style={gridStyle}>
+                        <Upload {...this.props.form}>
+                          <Button>
+                            <Icon type="upload" /> Wählerliste importieren
+                          </Button>
+                        </Upload>
+                      </Card.Grid>
+                    )}
+                  </FormItem>
+                </div>
               </Col>
+
               <Col span={8}>
-                <FormItem>
-                  {getFieldDecorator("start_date", {
-                    rules: [
-                      {
-                        required: true,
-                        message: "Bitte Parteienliste importieren!"
-                      }
-                    ]
-                  })(
-                    <Card.Grid style={gridStyle}>
-                      {" "}
-                      <Upload {...this.props.form}>
-                        <Button>
-                          <Icon type="upload" /> Parteienliste importieren
-                        </Button>
-                      </Upload>
-                    </Card.Grid>
-                  )}
-                </FormItem>
+                <div className={cx("show2")}>
+                  <FormItem>
+                    {getFieldDecorator("parties", {
+                      rules: [
+                        {
+                          message: "Bitte Parteienliste importieren!",
+                          required: true
+                        }
+                      ]
+                    })(
+                      <Card.Grid style={gridStyle}>
+                        <Upload {...this.props.form}>
+                          <Button>
+                            <Icon type="upload" /> Parteienliste importieren
+                          </Button>
+                        </Upload>
+                      </Card.Grid>
+                    )}
+                  </FormItem>
+                </div>
               </Col>
+
               <Col span={8}>
-                <FormItem>
-                  {getFieldDecorator("start_date", {
-                    rules: [
-                      {
-                        required: true,
-                        message: "Bitte Kandidatenliste importieren!"
-                      }
-                    ]
-                  })(
-                    <Card.Grid style={gridStyle}>
-                      {" "}
-                      <Upload {...this.props.form}>
-                        <Button>
-                          <Icon type="upload" /> Kandidatenliste importieren
-                        </Button>
-                      </Upload>
-                    </Card.Grid>
-                  )}
-                </FormItem>
+                <div className={cx("hidden3")}>
+                  <FormItem>
+                    {getFieldDecorator("candidats", {
+                      rules: [
+                        {
+                          message: "Bitte Kandidatenliste importieren!",
+                          required: true
+                        }
+                      ]
+                    })(
+                      <Card.Grid style={gridStyle}>
+                        <Upload {...this.props.form}>
+                          <Button>
+                            <Icon type="upload" /> Kandidatenliste importieren
+                          </Button>
+                        </Upload>
+                      </Card.Grid>
+                    )}
+                  </FormItem>
+                </div>
+                <div className={cx("hidden4")}>
+                  <FormItem>
+                    {getFieldDecorator("topic", {
+                      rules: [
+                        {
+                          message: "Bitte Themenliste importieren!",
+                          required: true
+                        }
+                      ]
+                    })(
+                      <Card.Grid style={gridStyle}>
+                        <Upload {...this.props.form}>
+                          <Button>
+                            <Icon type="upload" />Themenliste importieren
+                          </Button>
+                        </Upload>
+                      </Card.Grid>
+                    )}
+                  </FormItem>
+                </div>
               </Col>
             </Row>
             <Row>
