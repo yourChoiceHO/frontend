3c7e031ad0fbe0d8e15b2fe7bc17c5fc9b38724a
Jonas Hauß <haussjonas@gmail.com>
2018-05-27 17:02:04 +0200

    Externalized the css declarations of page styles

--- src/components/templates/Page.tsx
+++ src/components/templates/Page.tsx
@@ -1,14 +1,19 @@
 import { Layout } from "antd";
+import classNames from "classnames/bind";
 import React, { SFC } from "react";
 
 import Content from "@/components/organisms/Content";
+
+import styles from "./styles/page.module.less";
+
+const cx = classNames.bind(styles);
 
 interface IPageTemplateProps {
   header: JSX.Element;
 }
 
 const PageTemplate: SFC<IPageTemplateProps> = ({ header, children }) => (
-  <Layout style={{ minHeight: "100vh" }}>
+  <Layout className={cx("page")}>
     <Layout>
       {header && header}
       <Content>{children}</Content>
