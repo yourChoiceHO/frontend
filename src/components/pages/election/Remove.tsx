5753c13ee9e5f400fa8dbea354f235af625b4906
Jonas K <jkirchho@stud.hs-offenburg.de>
2018-06-08 16:43:30 +0200

    Remove added
    first draft

--- src/components/pages/election/Remove.tsx
+++ src/components/pages/election/Remove.tsx
@@ -1,7 +1,57 @@
-import React, { SFC } from "react";
+import { message, Popconfirm } from "antd";
+import { Cancel } from "fluture";
+import { isEmpty, pathOr } from "ramda";
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
 
-const ElectionRemove: SFC<{}> = () => {
-  return <div>Wahl löschen</div>;
-};
+import connect from "@/containers/connect";
+import ElectionContainer from "@/containers/Election";
+import { noop } from "@/utils";
 
-export default ElectionRemove;
+class ElectionRemove extends Component<{ election: ElectionContainer }> {
+  private cancel: Cancel = noop;
+
+  public componentDidMount() {
+    this.cancel = this.props.election.get(this.props.computedMatch.params.id);
+  }
+
+  public componentWillUnmount() {
+    this.cancel();
+  }
+
+  public confirmPop(e: any) {
+    const election = pathOr({}, ["state", "election", 0], this.props.election);
+
+    console.log(election);
+    // this.props.election.remove(election.id_election);
+    // election.remove(election.id_election);
+    message.success("Wahl gelöscht");
+  }
+
+  public cancelPop(e: any) {
+    message.warning("Vorgang abgebrochen");
+  }
+
+  public render() {
+    const election = pathOr({}, ["state", "election", 0], this.props.election);
+    if (isEmpty(election)) {
+      return <div />;
+    }
+
+    return (
+      <Popconfirm
+        title="Sind Sie sicher, dass Sie diese Wahl löschen wollen?"
+        onConfirm={this.confirmPop}
+        onCancel={this.cancelPop}
+        okText="Ja"
+        cancelText="Nein"
+      >
+        <a href="#">Löschen</a>
+      </Popconfirm>
+    );
+  }
+}
+
+export default connect({
+  election: ElectionContainer
+})(ElectionRemove);
