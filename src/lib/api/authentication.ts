9bdb6a514c01722ae4598329a98ad7e306fe7755
Jonas Hauß <haussjonas@gmail.com>
2018-06-17 18:51:48 +0200

    Replaced the api login urls with the correct routes of the backend

--- src/lib/api/authentication.ts
+++ src/lib/api/authentication.ts
@@ -5,11 +5,11 @@ import { IRequestError } from "@/types/error";
 import { IUserEntity, IVoterEntity } from "@/types/model";
 
 export function loginUser(credentials: any) {
-  return http.post<IUserEntity>("/user/authenticate", credentials);
+  return http.post<IUserEntity>("/login/user", credentials);
 }
 
 export function loginVoter(credentials: any) {
-  return http.post<IVoterEntity>("/voter/authenticate", credentials);
+  return http.post<IVoterEntity>("/login/voter", credentials);
 }
 
 export function logout(): Future<IRequestError, {}> {
