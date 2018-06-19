6c87494f76835a5e7017fb2202ccc9a752eb80b3
Jonas Hauß <haussjonas@gmail.com>
2018-06-19 15:09:22 +0200

    Time validation rules were applied (Regarding: https://trello.com/c/hfm49Um3/28-bug-datumsbeschr%C3%A4nkungen-nicht-vorhanden)

--- src/components/molecules/ElectionForm.tsx
+++ src/components/molecules/ElectionForm.tsx
@@ -12,6 +12,10 @@ import {
   message
 } from "antd";
 
+import locale from "antd/lib/date-picker/locale/de_DE";
+
+import moment from "moment";
+
 import connect from "@/containers/connect";
 
 import AuthenticationContainer from "@/containers/Authentication";
@@ -106,7 +110,7 @@ class ElectionForm extends Component<IUserFormProps> {
   public render() {
     const {
       election: { state },
-      form: { getFieldDecorator },
+      form: { getFieldDecorator, getFieldValue },
       ...props
     } = this.props;
 
@@ -151,227 +155,252 @@ class ElectionForm extends Component<IUserFormProps> {
     const showTopicList = reduceWithOr([isReferendum]);
 
     return (
-      <Form onSubmit={this.handleSubmit} layout="horizontal">
+      <Form onSubmit={this.handleSubmit} layout="vertical">
         <Card>
           <Row>
             <Col span={8}>
-              <FormItem>
-                <Card.Grid className={cx("grid")}>
-                  {getFieldDecorator("typ", {
-                    rules: [
-                      {
-                        message: "Bitte Wahltyp auswählen!",
-                        required: true
-                      }
-                    ]
-                  })(
-                    <RadioGroup disabled={isSaved}>
-                      <Radio
-                        className={cx("radio")}
-                        value={ElectionTypes.Europawahl}
-                      >
-                        Europawahl
-                      </Radio>
-                      <Radio
-                        className={cx("radio")}
-                        value={ElectionTypes.Bundestagswahl}
-                      >
-                        Bundestagswahl
-                      </Radio>
-                      <Radio
-                        className={cx("radio")}
-                        value={ElectionTypes.Landtagswahl}
-                      >
-                        Landtagswahl
-                      </Radio>
-                      <Radio
-                        className={cx("radio")}
-                        value={ElectionTypes.Buergermeisterwahl}
-                      >
-                        Bürgermeisterwahl
-                      </Radio>
-                      <Radio
-                        className={cx("radio")}
-                        value={ElectionTypes.Referendum}
-                      >
-                        Referendum
-                      </Radio>
-                      <Radio
-                        className={cx("radio")}
-                        value={ElectionTypes.Kommunalwahl}
-                      >
-                        Kommunalwahl
-                      </Radio>
-                      <Radio
-                        className={cx("radio")}
-                        value={ElectionTypes.LandtagswahlBW}
-                      >
-                        Landtagswahl (BW)
-                      </Radio>
-                      <Radio
-                        className={cx("radio")}
-                        value={ElectionTypes.LandtagswahlSL}
-                      >
-                        Landtagswahl (SL)
-                      </Radio>
-                    </RadioGroup>
-                  )}
-                </Card.Grid>
+              <FormItem label="Wahlart">
+                {getFieldDecorator("typ", {
+                  rules: [
+                    {
+                      message: "Bitte Wahltyp auswählen!",
+                      required: true
+                    }
+                  ]
+                })(
+                  <RadioGroup disabled={isSaved}>
+                    <Radio
+                      className={cx("radio")}
+                      value={ElectionTypes.Europawahl}
+                    >
+                      Europawahl
+                    </Radio>
+                    <Radio
+                      className={cx("radio")}
+                      value={ElectionTypes.Bundestagswahl}
+                    >
+                      Bundestagswahl
+                    </Radio>
+                    <Radio
+                      className={cx("radio")}
+                      value={ElectionTypes.Landtagswahl}
+                    >
+                      Landtagswahl
+                    </Radio>
+                    <Radio
+                      className={cx("radio")}
+                      value={ElectionTypes.Buergermeisterwahl}
+                    >
+                      Bürgermeisterwahl
+                    </Radio>
+                    <Radio
+                      className={cx("radio")}
+                      value={ElectionTypes.Referendum}
+                    >
+                      Referendum
+                    </Radio>
+                    <Radio
+                      className={cx("radio")}
+                      value={ElectionTypes.Kommunalwahl}
+                    >
+                      Kommunalwahl
+                    </Radio>
+                    <Radio
+                      className={cx("radio")}
+                      value={ElectionTypes.LandtagswahlBW}
+                    >
+                      Landtagswahl (BW)
+                    </Radio>
+                    <Radio
+                      className={cx("radio")}
+                      value={ElectionTypes.LandtagswahlSL}
+                    >
+                      Landtagswahl (SL)
+                    </Radio>
+                  </RadioGroup>
+                )}
               </FormItem>
             </Col>
             <Col span={8}>
-              <FormItem>
-                <Card.Grid className={cx("grid")}>
-                  Endzeitpunkt
-                  {getFieldDecorator("start_date", {
-                    rules: [
-                      {
-                        message: "Bitte Startzeitpunkt festlegen!",
-                        required: true
+              <FormItem label="Startzeitpunkt">
+                {getFieldDecorator("start_date", {
+                  rules: [
+                    {
+                      message: "Bitte Startzeitpunkt festlegen!",
+                      required: true
+                    },
+                    // {
+                    //   message:
+                    //     "Startzeitpunkt muss vor dem Endzeitpunkt liegen",
+                    //   validator: (rule, value, callback) => {
+                    //     const end = getFieldValue("end_date");
+                    //     const isBefore = value.isBefore(end);
+
+                    //     if (isBefore) {
+                    //       callback();
+                    //     } else {
+                    //       callback(rule.message);
+                    //     }
+                    //   }
+                    // },
+                    {
+                      message:
+                        "Startzeitpunkt muss zwei Wochen vor dem Endzeitpunkt liegen",
+                      validator: (rule, value, callback) => {
+                        const end = getFieldValue("end_date");
+                        const diff = value.diff(end, "week");
+
+                        if (diff === -2) {
+                          callback();
+                        } else {
+                          callback(rule.message);
+                        }
                       }
-                    ]
-                  })(
-                    <DatePicker
-                      disabled={isSaved}
-                      format="YYYY-MM-DD HH:mm:ss"
-                    />
-                  )}
-                </Card.Grid>
+                    }
+                  ]
+                })(
+                  <DatePicker
+                    locale={locale}
+                    showTime={{ defaultValue: moment("18:00:00", "HH:mm:ss") }}
+                    disabled={isSaved}
+                    format="YYYY-MM-DD HH:mm:ss"
+                  />
+                )}
               </FormItem>
             </Col>
             <Col span={8}>
-              <FormItem>
-                <Card.Grid className={cx("grid")}>
-                  Endzeitpunkt
-                  {getFieldDecorator("end_date", {
-                    rules: [
-                      {
-                        message: "Bitte Endzeitpunkt festlegen!",
-                        required: true
+              <FormItem label="Endzeitpunkt">
+                {getFieldDecorator("end_date", {
+                  rules: [
+                    {
+                      message: "Bitte Endzeitpunkt festlegen!",
+                      required: true
+                    },
+                    {
+                      message: "Endzeitpunkt muss auf 18:00 Uhr gesetzt werden",
+                      validator: (rule, value, callback) => {
+                        const hours = value.hours();
+                        const minutes = value.minutes();
+
+                        if (hours === 18 && minutes === 0) {
+                          callback();
+                        } else {
+                          callback(rule.message);
+                        }
                       }
-                    ]
-                  })(
-                    <DatePicker
-                      disabled={isSaved}
-                      format="YYYY-MM-DD HH:mm:ss"
-                    />
-                  )}
-                </Card.Grid>
+                    }
+                  ]
+                })(
+                  <DatePicker
+                    locale={locale}
+                    showTime={{ defaultValue: moment("18:00:00", "HH:mm:ss") }}
+                    disabled={isSaved}
+                    format="YYYY-MM-DD HH:mm:ss"
+                  />
+                )}
               </FormItem>
             </Col>
           </Row>
           <Row>
-            <FormItem>
-              <Card.Grid className={cx("grid")}>
-                {getFieldDecorator("text", {})(
-                  <Input
-                    disabled={isSaved}
-                    placeholder="Weitere Informationen zur Wahl"
-                  />
-                )}
-              </Card.Grid>
+            <FormItem label="Beschreibung">
+              {getFieldDecorator("text")(
+                <Input
+                  disabled={isSaved}
+                  placeholder="Weitere Informationen zur Wahl"
+                />
+              )}
             </FormItem>
           </Row>
           <Row>
             <Col span={8}>
-              <FormItem>
-                <Card.Grid className={cx("grid")}>
-                  {getFieldDecorator("voters", {
+              <FormItem label="Wählerliste">
+                {getFieldDecorator("voters", {
+                  rules: [
+                    {
+                      message: "Bitte Wählerliste importieren!",
+                      required: true
+                    }
+                  ]
+                })(
+                  <Upload
+                    customRequest={this.props.election.addVoters(electionId)}
+                    disabled={isNew}
+                  >
+                    <Button disabled={isNew} icon="upload">
+                      Wählerliste importieren
+                    </Button>
+                  </Upload>
+                )}
+              </FormItem>
+            </Col>
+            <Col span={8}>
+              {showPartyList && (
+                <FormItem label="Parteienliste">
+                  {getFieldDecorator("parties", {
                     rules: [
                       {
-                        message: "Bitte Wählerliste importieren!",
+                        message: "Bitte Parteienliste importieren!",
                         required: true
                       }
                     ]
                   })(
                     <Upload
-                      customRequest={this.props.election.addVoters(electionId)}
+                      customRequest={this.props.election.addParties(electionId)}
                       disabled={isNew}
                     >
                       <Button disabled={isNew} icon="upload">
-                        Wählerliste importieren
+                        Parteienliste importieren
                       </Button>
                     </Upload>
                   )}
-                </Card.Grid>
-              </FormItem>
-            </Col>
-            <Col span={8}>
-              {showPartyList && (
-                <FormItem>
-                  <Card.Grid className={cx("grid")}>
-                    {getFieldDecorator("parties", {
-                      rules: [
-                        {
-                          message: "Bitte Parteienliste importieren!",
-                          required: true
-                        }
-                      ]
-                    })(
-                      <Upload
-                        customRequest={this.props.election.addParties(
-                          electionId
-                        )}
-                        disabled={isNew}
-                      >
-                        <Button disabled={isNew} icon="upload">
-                          Parteienliste importieren
-                        </Button>
-                      </Upload>
-                    )}
-                  </Card.Grid>
                 </FormItem>
               )}
             </Col>
             <Col span={8}>
               {showCandidateList && (
-                <FormItem>
-                  <Card.Grid className={cx("grid")}>
-                    {getFieldDecorator("candidates", {
-                      rules: [
-                        {
-                          message: "Bitte Kandidatenliste importieren!",
-                          required: true
-                        }
-                      ]
-                    })(
-                      <Upload
-                        customRequest={this.props.election.addCandidates(
-                          electionId
-                        )}
-                        disabled={isNew}
-                      >
-                        <Button disabled={isNew} icon="upload">
-                          Kandidatenliste importieren
-                        </Button>
-                      </Upload>
-                    )}
-                  </Card.Grid>
+                <FormItem label="Kandidatenliste">
+                  {getFieldDecorator("candidates", {
+                    rules: [
+                      {
+                        message: "Bitte Kandidatenliste importieren!",
+                        required: true
+                      }
+                    ]
+                  })(
+                    <Upload
+                      customRequest={this.props.election.addCandidates(
+                        electionId
+                      )}
+                      disabled={isNew}
+                    >
+                      <Button disabled={isNew} icon="upload">
+                        Kandidatenliste importieren
+                      </Button>
+                    </Upload>
+                  )}
                 </FormItem>
               )}
               {showTopicList && (
-                <FormItem>
-                  <Card.Grid className={cx("grid")}>
-                    {getFieldDecorator("topic", {
-                      rules: [
-                        {
-                          message: "Bitte Themenliste importieren!",
-                          required: true
-                        }
-                      ]
-                    })(
-                      <Upload
-                        customRequest={this.props.election.addReferendums(
-                          electionId
-                        )}
-                        disabled={isNew}
-                      >
-                        <Button disabled={isNew} icon="upload">
-                          Themenliste importieren
-                        </Button>
-                      </Upload>
-                    )}
-                  </Card.Grid>
+                <FormItem label="Themenliste">
+                  {getFieldDecorator("topic", {
+                    rules: [
+                      {
+                        message: "Bitte Themenliste importieren!",
+                        required: true
+                      }
+                    ]
+                  })(
+                    <Upload
+                      customRequest={this.props.election.addReferendums(
+                        electionId
+                      )}
+                      disabled={isNew}
+                    >
+                      <Button disabled={isNew} icon="upload">
+                        Themenliste importieren
+                      </Button>
+                    </Upload>
+                  )}
                 </FormItem>
               )}
             </Col>
